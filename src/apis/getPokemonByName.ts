b931d77b2e2f4e31b46274510618cd4dd1ad2420
KolyaHarutyunyan <kolyah2809@gmail.com>
2022-11-27 07:58:02 +0400

    hot fix

--- src/apis/getPokemonByName.ts
+++ src/apis/getPokemonByName.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 import IPokemonInfo from "../interfaces/IPokemonInfo";
 
 export const getPokemonByName = async (name: string): Promise<IPokemonInfo> => {
-  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
   return data;
 };
