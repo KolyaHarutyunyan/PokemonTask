df35732b513132e6924727703d04004a6f44153e
KolyaHarutyunyan <kolyah2809@gmail.com>
2022-11-27 06:36:30 +0400

    u1

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const http = require('http')
-const fs = require('fs')
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  const filePath = __dirname + (req.url === '/' ? '/index.html' : req.url)
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404)
-      res.end(JSON.stringify(err))
-      return
+http
+  .createServer((req, res) => {
+    let filePath = path.join(__dirname, "dist",(req.url === "/" ? "/index.html" : req.url));
+
+    if (req.url.endsWith(".js")) {
+      filePath = path.join(__dirname, "dist", req.url);
+      return fs.readFile(filePath, (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+
+        res.writeHead(200, { "Content-Type": "text/javascript" });
+        res.end(data);
+      });
     }
-    res.writeHead(200)
-    res.end(data)
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
   })
-}).listen(port)
+  .listen(port);
