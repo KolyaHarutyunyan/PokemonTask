8246b618151656710216f91fe277ac6c5ee96bed
KolyaHarutyunyan <kolyah2809@gmail.com>
2022-11-27 09:43:37 +0400

    hot fix

--- src/components/PaginationHOC.tsx
+++ src/components/PaginationHOC.tsx
@@ -2,38 +2,43 @@ import "./PaginationHOC.css";
 import Pagination from "@mui/material/Pagination";
 
 interface IPaginationHOCProps {
-    count: number;
-    handleChangePage: (value: number) => void;
-    page: number;
-    listLength: number;
-    limitCountNumber: number;
+  count: number;
+  handleChangePage: (value: number) => void;
+  page: number;
+  listLength: number;
+  limitCountNumber: number;
 }
 
 const PaginationHOC: React.FC<IPaginationHOCProps> = ({
-   count,
-   handleChangePage,
-   page,
-   listLength,
-   limitCountNumber = 1,
+  count,
+  handleChangePage,
+  page,
+  listLength,
+  limitCountNumber = 1,
 }) => {
-   const firsCountNumber = page <= 1 ? 1 : (page - 1) * limitCountNumber + 1;
-   const shownCountNumber = page <= 1 ? listLength : firsCountNumber - 1 + listLength;
-   
-   return (
-      <div className="paginationWrapper">
-         <div>
-            <p className="showCountText">
-               {`Showing ${firsCountNumber} to ${shownCountNumber} of ${count} entries`}
-            </p>
-         </div>
-         <Pagination
-            onChange={(event, value) => handleChangePage(value)}
-            page={page}
-            count={count && Math.ceil(count / limitCountNumber)}
-            color={"primary"}
-         />
+  const firsCountNumber = page <= 1 ? 1 : (page - 1) * limitCountNumber + 1;
+  const shownCountNumber =
+    page <= 1 ? listLength : firsCountNumber - 1 + listLength;
+
+  return (
+    <div className="paginationWrapper">
+      <div>
+        <p className="showCountText">
+          {`Showing ${count === 1 ? count : firsCountNumber} to ${
+            count === 1 ? count : shownCountNumber
+          } of ${count} entries`}
+        </p>
       </div>
-   );
+      <Pagination
+        onChange={(event, value) => handleChangePage(value)}
+        page={page}
+        count={
+          count && Math.ceil(count / count === 1 ? count : limitCountNumber)
+        }
+        color={"primary"}
+      />
+    </div>
+  );
 };
 
 export default PaginationHOC;
--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -13,145 +13,166 @@ import { getPokemonByName } from "../apis/getPokemonByName";
 import { getCurrentPokemon } from "../apis/getCurrentPokemon";
 
 const Main = () => {
-    const [pokemonData, setPokemonData] = useState<IPokemonData>({count: 0, results: []});
-    const [open, setOpen] = useState(false);
-    const [currentPokemon, setCurrentPokemon] = useState<IPokemonInfo | undefined>();
-    const [searchedValue, setSearchedValue] = useState("");
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(false);
+  const [pokemonData, setPokemonData] = useState<IPokemonData>({
+    count: 0,
+    results: [],
+  });
+  const [open, setOpen] = useState(false);
+  const [currentPokemon, setCurrentPokemon] = useState<
+    IPokemonInfo | undefined
+  >();
+  const [searchedValue, setSearchedValue] = useState("");
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
-    const fetchData = (offset?: number, limit?: number) => {
-        setLoading(true);
-        getPokemons(offset, limit)
-            .then((response: IPokemonData) => {
-                const data: IPokemonData = {
-                    count: response.count,
-                    results: response.results
-                };
-                const urls = data.results?.map(result => result.url);
-                if (urls) {
-                    setLoading(true);
-                    Promise.all(urls?.map(url => getCurrentPokemon(url)))
-                        .then((response: IPokemonInfo[]) => {
-                            const pokemonData: IPokemonData = {
-                                count: data.count,
-                                results: response.map((pokemon) => ({
-                                    ...pokemon,
-                                    name: pokemon.species.name,
-                                    url: pokemon.species.url,
-                                    imgUrl: pokemon.sprites.front_default
-                                }))
-                            };
-                            setPokemonData(pokemonData);
-                        })
-                        .catch(err => console.error(err))
-                        .finally(() => setLoading(false));
-                }
+  const fetchData = (offset?: number, limit?: number) => {
+    setLoading(true);
+    getPokemons(offset, limit)
+      .then((response: IPokemonData) => {
+        const data: IPokemonData = {
+          count: response.count,
+          results: response.results,
+        };
+        const urls = data.results?.map((result) => result.url);
+        if (urls) {
+          setLoading(true);
+          Promise.all(urls?.map((url) => getCurrentPokemon(url)))
+            .then((response: IPokemonInfo[]) => {
+              const pokemonData: IPokemonData = {
+                count: data.count,
+                results: response.map((pokemon) => ({
+                  ...pokemon,
+                  name: pokemon.species.name,
+                  url: pokemon.species.url,
+                  imgUrl: pokemon.sprites.front_default,
+                })),
+              };
+              setPokemonData(pokemonData);
             })
-            .catch(err => console.error(err))
-    };
-
-    useEffect(() => fetchData(), []);
-    
-    useEffect(() => {
-        if (searchedValue.trim().length >= 3) {
-            const timerId = setTimeout(() => {
-                setLoading(true);
-                getPokemonByName(searchedValue)
-                    .then((response: IPokemonInfo) => {
-                        const data: IPokemonData = {
-                            count: 0,
-                            results: [{
-                                ...response,
-                                name: response.species.name,
-                                url: response.species.url,
-                                imgUrl: response.sprites.front_default
-                            }]
-                        };
-                        setPokemonData(data)
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        const data: IPokemonData = {
-                            count: 0,
-                            results: []
-                        };
-                        setPokemonData(data);
-                    })
-                    .finally(() => setLoading(false));
-            }, 700);
-            return () => {
-                if (timerId) clearTimeout(timerId);
-            };
-        } else {
-            if (pokemonData.count === 0) {
-                const timerId = setTimeout(fetchData, 700);
-                return () => {
-                    if (timerId) clearTimeout(timerId);
-                };
-            }
+            .catch((err) => console.error(err))
+            .finally(() => setLoading(false));
         }
-    }, [searchedValue]);
+      })
+      .catch((err) => console.error(err));
+  };
 
-    const _limitCount = 16;
+  useEffect(() => fetchData(), []);
 
-    const changePageHandler = (number: number): void => {
-        if (page === number) return;
-        const { offset, limit } = getOffsetAndLimit(number, _limitCount, pokemonData.count);
+  useEffect(() => {
+    if (searchedValue.trim().length >= 3) {
+      const timerId = setTimeout(() => {
         setLoading(true);
-        fetchData(offset, limit);
-        setPage(number);
-    };
+        getPokemonByName(searchedValue)
+          .then((response: IPokemonInfo) => {
+            const data: IPokemonData = {
+              count: 1,
+              results: [
+                {
+                  ...response,
+                  name: response.species.name,
+                  url: response.species.url,
+                  imgUrl: response.sprites.front_default,
+                },
+              ],
+            };
+            setPokemonData(data);
+          })
+          .catch((err) => {
+            console.error(err);
+            const data: IPokemonData = {
+              count: 0,
+              results: [],
+            };
+            setPokemonData(data);
+          })
+          .finally(() => setLoading(false));
+      }, 700);
+      return () => {
+        if (timerId) clearTimeout(timerId);
+      };
+    } else {
+      if (pokemonData.count === 0 || pokemonData.count === 1) {
+        const timerId = setTimeout(fetchData, 700);
+        return () => {
+          if (timerId) clearTimeout(timerId);
+        };
+      }
+    }
+  }, [searchedValue]);
 
-    return (
-        <>
-            <main className="main">
-                <div className="searchbox">
-                    <input
-                        type="text"
-                        className="search-input"
-                        value={searchedValue}
-                        placeholder="Search pokemon..."
-                        onChange={(e) => setSearchedValue(e.target.value)}
-                    />
-                </div>
-                {loading
-                    ? <CircularProgress size={50} color={"primary"} className={"progress"} />
-                    : pokemonData.results?.length === 0 
-                        ? <p className="no-items-text">No Pokemons Found</p>
-                        : (
-                            <>
-                                <div className="cards">
-                                    <div className="cards-box">
-                                        {pokemonData.results?.map((item) => (
-                                            <Card
-                                                key={item.url}
-                                                card={item}
-                                                onViewMore={(card: IPokemonInfo): void => {
-                                                    setCurrentPokemon(card);
-                                                    setOpen(true);
-                                                }}
-                                            />
-                                        ))}
-                                    </div>
-                                </div>
-                                {pokemonData.count !== 0 && (
-                                    <PaginationHOC
-                                        listLength={pokemonData.results?.length || 0}
-                                        page={page}
-                                        handleChangePage={(number) => changePageHandler(number)}
-                                        count={pokemonData.count}
-                                        limitCountNumber={_limitCount}
-                                    />
-                                )}
-                            </>
-                        )}
-            </main>
-            <MUIModal isOpen={open} closeModal={() => setOpen(false)}>
-                <PokemonInfo pokemonInfo={currentPokemon} />
-            </MUIModal>
-        </>
+  const _limitCount = 16;
+
+  const changePageHandler = (number: number): void => {
+    if (page === number) return;
+    const { offset, limit } = getOffsetAndLimit(
+      number,
+      _limitCount,
+      pokemonData.count
     );
+    setLoading(true);
+    fetchData(offset, limit);
+    setPage(number);
+  };
+
+  return (
+    <>
+      <main className="main">
+        <div className="searchbox">
+          <input
+            type="text"
+            className="search-input"
+            value={searchedValue}
+            placeholder="Search pokemon..."
+            onChange={(e) => setSearchedValue(e.target.value)}
+          />
+        </div>
+        {loading ? (
+          <CircularProgress
+            size={50}
+            color={"primary"}
+            className={"progress"}
+          />
+        ) : pokemonData.results?.length === 0 ? (
+          <p className="no-items-text">No Pokemons Found</p>
+        ) : (
+          <>
+            <div className="cards">
+              <div className="cards-box">
+                {pokemonData.results?.map((item) => (
+                  <Card
+                    key={item.url}
+                    card={item}
+                    onViewMore={(card: IPokemonInfo): void => {
+                      setCurrentPokemon(card);
+                      setOpen(true);
+                    }}
+                  />
+                ))}
+              </div>
+            </div>
+            {pokemonData.count !== 0 && (
+              <PaginationHOC
+                listLength={
+                  pokemonData.count === 1
+                    ? pokemonData.count
+                    : pokemonData.results?.length || 0
+                }
+                page={page}
+                handleChangePage={(number) => changePageHandler(number)}
+                count={pokemonData.count}
+                limitCountNumber={
+                  pokemonData.count === 1 ? pokemonData.count : _limitCount
+                }
+              />
+            )}
+          </>
+        )}
+      </main>
+      <MUIModal isOpen={open} closeModal={() => setOpen(false)}>
+        <PokemonInfo pokemonInfo={currentPokemon} />
+      </MUIModal>
+    </>
+  );
 };
 
 export default Main;
